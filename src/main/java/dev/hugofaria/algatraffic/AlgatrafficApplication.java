2cc7e140bb3f78bc20785606e2b26550217a9552
Hugo Faria <hugofaria157@live.com>
2024-01-15 11:20:27 -0300

    estrutura inicial do projeto

---
+++ src/main/java/dev/hugofaria/algatraffic/AlgatrafficApplication.java
@@ -0,0 +1,13 @@
+package dev.hugofaria.algatraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgatrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgatrafficApplication.class, args);
+	}
+
+}
