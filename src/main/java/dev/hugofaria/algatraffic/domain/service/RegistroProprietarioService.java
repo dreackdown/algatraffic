ac9ff25b293d8f74fd1d8c844996d287a1113071
Hugo Faria <hugofaria157@live.com>
2024-01-17 00:23:20 -0300

    adicionando funcionalidades

---
+++ src/main/java/dev/hugofaria/algatraffic/domain/service/RegistroProprietarioService.java
@@ -0,0 +1,34 @@
+package dev.hugofaria.algatraffic.domain.service;
+
+import dev.hugofaria.algatraffic.domain.exception.NegocioException;
+import dev.hugofaria.algatraffic.domain.model.Proprietario;
+import dev.hugofaria.algatraffic.domain.repository.ProprietarioRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class RegistroProprietarioService {
+
+    private final ProprietarioRepository proprietarioRepository;
+
+    @Transactional
+    public Proprietario salvar(Proprietario proprietario) {
+
+        boolean emailEmUso = proprietarioRepository.findByEmail(proprietario.getEmail())
+                .filter((p -> !p.equals(proprietario)))
+                .isPresent();
+
+        if (emailEmUso) {
+            throw new NegocioException("Já existe um proprietário cadastrado com este e-mail.");
+        }
+
+        return proprietarioRepository.save(proprietario);
+    }
+
+    @Transactional
+    public void excluir(Long proprietarioId) {
+        proprietarioRepository.deleteById(proprietarioId);
+    }
+}
