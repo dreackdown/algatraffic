5ac2c9cd9f10b361bfc2c81aac4804aaef3ca523
Hugo Faria <hugofaria157@live.com>
2024-01-22 19:34:17 -0300

    adicionando DTO de veiculo

---
+++ src/main/java/dev/hugofaria/algatraffic/api/model/VeiculoDTO.java
@@ -0,0 +1,21 @@
+package dev.hugofaria.algatraffic.api.model;
+
+import dev.hugofaria.algatraffic.domain.model.StatusVeiculo;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class VeiculoDTO {
+
+    private Long id;
+    private String nomeProprietario;
+    private String marca;
+    private String modelo;
+    private String numeroPlaca;
+    private StatusVeiculo status;
+    private OffsetDateTime dataCadastro;
+    private OffsetDateTime dataApreensao;
+}
