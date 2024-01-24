ef2389cbe86191372dd3fbbfe907e1b64d68793b
Hugo Faria <hugofaria157@live.com>
2024-01-24 02:03:58 -0300

    adicionando exception EntidadeNaoEncontrada

---
+++ src/main/java/dev/hugofaria/algatraffic/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,8 @@
+package dev.hugofaria.algatraffic.domain.exception;
+
+public class EntidadeNaoEncontradaException extends NegocioException {
+
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
